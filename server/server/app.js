3547c051132dffdb5cc8182f804288ddc7ca3391
andrewbidylo <77558037+andrewbidylo@users.noreply.github.com>
2022-05-10 16:23:00 -0700

    initialize the serveer

---
+++ server/server/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const {graphqlHTTP} = require('express-graphql');
+
+const app = express();
+const PORT = 8000;
+
+app.use('/graphql', graphqlHTTP({}));
+
+app.listen(PORT, err => {
+  err ? console.log(err) : console.log('Server started!');
+});
