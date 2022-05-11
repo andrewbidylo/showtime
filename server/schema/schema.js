fafcc150863da2da70826d493c69dd0f48428f91
andrewbidylo <77558037+andrewbidylo@users.noreply.github.com>
2022-05-10 17:39:56 -0700

    add director type, queryand data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,13 +1,19 @@
 const graphql = require('graphql')
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLID } = graphql
 
 const movies = [
-  { id : '1', name: 'Pulp Fiction', genre: 'Crime'},
-  { id : '2', name: '1984', genre: 'Sci-Fi'},
-  { id : '3', name: 'V for vendetta', genre: 'Sci-Fi-Triller'},
-  { id : '4', name: 'Snatch', genre: 'Crime-Comedy'},
+  { id: '1', name: 'Pulp Fiction', genre: 'Crime' },
+  { id: '2', name: '1984', genre: 'Sci-Fi' },
+  { id: '3', name: 'V for vendetta', genre: 'Sci-Fi-Triller' },
+  { id: '4', name: 'Snatch', genre: 'Crime-Comedy' },
+]
 
+const directors = [
+  { id: '1', name: 'Quentin Tatantino', age: 57 },
+  { id: '2', name: 'Michael Radford', age: 74 },
+  { id: '3', name: 'James McTeigue', age: 53 },
+  { id: '4', name: 'Guy Ritchie', age: 52 },
 ]
 
 const MovieType = new GraphQLObjectType({
@@ -19,6 +25,15 @@ const MovieType = new GraphQLObjectType({
   }),
 })
 
+const DirectorType = new GraphQLObjectType({
+  name: "Director",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  }),
+})
+
 const Query = new GraphQLObjectType({
   name: 'Query',
   fields: {
@@ -26,9 +41,17 @@ const Query = new GraphQLObjectType({
       type: MovieType,
       args: { id: { type: GraphQLID } },
       resolve(perent, args) {
-        return movies.find(movie=> movie.id == args.id)
-      }
+        return movies.find(movie => movie.id == args.id)
+      },
     },
+    director: {
+      type: DirectorType,
+      args: { id: { type: GraphQLID } },
+      resolve(perent, args) {
+        return directors.find(director => director.id == args.id)
+      },
+    },
+
   }
 })
 
